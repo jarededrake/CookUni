054275d22567b2a9775791944a28491df97ebe20
Jared Drake <jareddrake@Jareds-MacBook-Pro.local>
2020-08-10 21:17:57 -0600

    got registration and notify js to work

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,14 +14,6 @@ async function get(url) {
 //POST Function 
 
 async function post(url, data) {
-    console.log({
-        headers: {
-            Authorization: authToken,
-            "Content-Type": "application/json"
-        },
-        method: "POST",
-        body: data
-    })
     const response = await fetch(url, {
         headers: {
             Authorization: authToken,
@@ -77,4 +69,52 @@ function login() {
 }
 
 document.getElementById("signInButton").addEventListener("click", login)
+
+//Click function to make sign up appear
+
+document.getElementById("register").addEventListener("click", function() {
+    document.getElementById("signup").hidden = false;
+    if(document.getElementById("sign-in").hidden == false) {
+        document.getElementById("sign-in").hidden = true;
+    }
+});
+
+//Click function to submit new sign up 
+
+document.getElementById("signupbutton").addEventListener("click", function(){
+    let data = {"username": document.getElementById("signupusername").value,
+                "password": document.getElementById("signuppassword").value,
+                "firstName": document.getElementById("signupfirstname").value,
+                "lastName": document.getElementById("signuplastname").value
+            };
+    console.log(data)
+    fetch("https://baas.kinvey.com/user/kid_SJqu6rsWD/", {
+    method: "POST", 
+    body: JSON.stringify(data),
+    headers: {
+        "Authorization": "Basic " + btoa("kid_SJqu6rsWD:d1f460b6f7da483982c14860a3bfeed4"),
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+    }).then(response => {
+        if(response.ok) {
+            $.notify("Sign up succesful!", {
+                position:"t c"
+            });
+        } else {
+            $.notify("Sign up unsuccesful, please try again", {
+                position:"t c"
+            });
+        }
+        return response.json();
+    })
+});
+
+
+//Click function to mkae sign in appear
+document.getElementById("signin").addEventListener("click", function() {
+    document.getElementById("sign-in").hidden = false;
+    if(document.getElementById("signup").hidden == false) {
+        document.getElementById("signup").hidden = true;
+    }
+});
 
